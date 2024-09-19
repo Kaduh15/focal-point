fbcb2726af40d400635bc9e2f687b3a58c1c5a08
Kadu <kadu.silva2014@gmail.com>
2024-09-19 17:07:12 -0300

    feat: adiciona suporte para botão link no componente Button e ajusta estilos de hover para diferentes variantes de botão

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,14 +1,36 @@
+import Link from 'next/link'
 import style from './style.module.scss'
+
+type IsLink =
+  | {
+      isLink: boolean
+      href: string
+    }
+  | {
+      isLink?: boolean
+      href?: never
+    }
 
 export type ButtonProps = {
   variant?: 'primary' | 'secondary' | 'delete'
-} & React.ButtonHTMLAttributes<HTMLButtonElement>
+} & IsLink &
+  React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export function Button({
   variant = 'primary',
   children,
+  isLink = false,
+  href,
   ...buttonProps
 }: ButtonProps) {
+  if (isLink && href) {
+    return (
+      <Link className={style[variant]} href={href}>
+        {children}
+      </Link>
+    )
+  }
+
   return (
     <button className={style[variant]} {...buttonProps}>
       {children}
