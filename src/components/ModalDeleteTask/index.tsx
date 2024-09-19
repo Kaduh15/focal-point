93da3130aec10cea5e4191d0d69596063555d34c
Kadu <kadu.silva2014@gmail.com>
2024-09-19 12:18:23 -0300

    feat: adiciona componente ModalDeleteTask com botão para confirmar a deleção da tarefa e estilos correspondentes para a interface do usuário

---
+++ src/components/ModalDeleteTask/index.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { handleDeleteTaskAction } from '@/actions/handleDeleteTask'
+import { useRouter } from 'next/navigation'
+import { useServerAction } from 'zsa-react'
+import { Button } from '../ui/Button'
+import { Modal } from '../ui/Modal'
+import style from './style.module.scss'
+
+type ModalDeleteTaskProps = {
+  id: number
+}
+
+export function ModalDeleteTask({ id }: ModalDeleteTaskProps) {
+  const route = useRouter()
+
+  const { execute: deleteTask, isPending: deletePending } = useServerAction(
+    handleDeleteTaskAction,
+    {
+      onSuccess: () => {
+        route.back()
+      },
+    },
+  )
+
+  return (
+    <Modal>
+      <h2>Deletar</h2>
+      <p>Tem certeza que você deseja deletar essa tarefa?</p>
+      <div className={style.buttons}>
+        <Button
+          disabled={deletePending}
+          onClick={async () => await deleteTask({ id })}
+          variant="delete"
+        >
+          Deletar
+        </Button>
+        <Button onClick={route.back} variant="secondary">
+          Cancelar
+        </Button>
+      </div>
+    </Modal>
+  )
+}
