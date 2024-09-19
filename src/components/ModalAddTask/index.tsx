f132a5d4ae62e209a8b43850464947e9ef6cd5de
Kadu <kadu.silva2014@gmail.com>
2024-09-19 12:15:48 -0300

    feat: adiciona componente ModalAddTask com formulário de adição de tarefas e estilos correspondentes para a interface do usuário

---
+++ src/components/ModalAddTask/index.tsx
@@ -0,0 +1,18 @@
+import { FormAddTask } from '../FormAddTask'
+import { Button } from '../ui/Button'
+import { Modal } from '../ui/Modal'
+
+import style from './style.module.scss'
+
+export function ModalAddTask() {
+  return (
+    <Modal>
+      <h2>Nova tarefa</h2>
+      <FormAddTask id="add-test-form" />
+      <div className={style.buttons}>
+        <Button form="add-test-form">Adicionar</Button>
+        <Button variant="secondary">Cancelar</Button>
+      </div>
+    </Modal>
+  )
+}
